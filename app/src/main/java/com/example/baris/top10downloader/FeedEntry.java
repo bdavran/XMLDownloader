f79645f51b921226292a2aa2d3005333cb789166
bdavran <bardavran@gmail.com>
2017-01-20 20:14:45 +0300

    Initialise

---
+++ app/src/main/java/com/example/baris/top10downloader/FeedEntry.java
@@ -0,0 +1,56 @@
+package com.example.baris.top10downloader;
+
+/**
+ * Created by Baros on 12.11.2016.
+ */
+
+public class FeedEntry {
+
+    private String name;
+    private String artist;
+    private String releaseDate;
+    private String summary;
+    private String imageURL;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+
+}
